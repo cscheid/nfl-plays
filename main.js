916cc3926c5f46fb23e953ea20faa66c617e35e0
Carlos Scheidegger <cscheid@research.att.com>
2013-01-05 16:38:45 -0500

    some annotations, about, clickable selections

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $().ready(function() {
 
+    $("#help").click(function() {
+        $("#help").hide();
+    });
+
     var canvas = document.getElementById("webgl");
     var width = window.innerWidth, height = window.innerHeight;
     canvas.width = width;
@@ -58,7 +62,7 @@ $().ready(function() {
             );
 
         var unselected_color = Shade.vec(0,0,0,0.1);
-        var selected_color = Shade.parameter("vec4", vec.make([1,0,0,1]));
+        var selected_color = Shade.parameter("vec4", vec.make([1,0,0,0.5]));
         var selection_enabled = Shade.parameter("float", 0);
 
         var sel_completed   = Shade.parameter("float", 0);
@@ -80,9 +84,34 @@ $().ready(function() {
             Facet.Scene.invalidate();
         }
 
-        $("#intercepted").hover(make_selection({completed:  0, intercepted: 1, bias:  1}), remove_selection);
-        $("#complete")   .hover(make_selection({completed:  1, intercepted: 0, bias:  1}), remove_selection);
-        $("#incomplete") .hover(make_selection({completed: -1, intercepted: 0, bias: -1}), remove_selection);
+        var hover_out_function = remove_selection;
+        var previous_selection;
+
+        function fix_selection(fun) {
+            return function(evt) {
+                if (hover_out_function !== fun) {
+                    $(evt.target).addClass("selected");
+                    hover_out_function = fun;
+                    previous_selection = evt.target;
+                } else {
+                    $(previous_selection).removeClass("selected");
+                    previous_selection = undefined;
+                    hover_out_function = remove_selection;
+                }
+            };
+        }
+
+        var intercepted_selection = make_selection({completed:  0, intercepted: 1, bias:  1});
+        var complete_selection    = make_selection({completed:  1, intercepted: 0, bias:  1});
+        var incomplete_selection  = make_selection({completed: -1, intercepted: 0, bias: -1});
+
+        $("#intercepted").hover(intercepted_selection, function() { hover_out_function(); });
+        $("#complete")   .hover(complete_selection,    function() { hover_out_function(); });
+        $("#incomplete") .hover(incomplete_selection,  function() { hover_out_function(); });
+
+        $("#intercepted").click(fix_selection(intercepted_selection));
+        $("#complete")   .click(fix_selection(complete_selection));
+        $("#incomplete") .click(fix_selection(incomplete_selection));
 
         var sel_discriminant = 
             sel_completed.mul(completed).add(sel_intercepted.mul(intercepted));
@@ -91,6 +120,47 @@ $().ready(function() {
         var is_selected = selection_enabled.eq(1).and(selection_predicate);
 
         var final_color = Shade.ifelse(is_selected, selected_color, unselected_color);
+
+        Facet.Scene.add(Facet.Marks.lines({
+            elements: 10,
+            position: function(index, which) { 
+                var x = Shade.ifelse(which.eq(0), 0, 100);
+                return interactor.camera(Shade.vec(x, index.mul(2)));
+            },
+            color: function() { return Shade.vec(0,0,0,0.2); }
+        }));
+
+        Facet.Net.json("data/opensans.regular.json", function(font) {
+            var pts = [
+                Shade.vec(0,20.5),
+                Shade.vec(25,20.5),
+                Shade.vec(50,20.5),
+                Shade.vec(75,20.5),
+                Shade.vec(100,20.5),
+                Shade.vec(-0.2,0-0.5),
+                Shade.vec(-0.2,4-0.5),
+                Shade.vec(-0.2,16-0.5)
+            ];
+            var scale = [1.5, 1.5, 1.5, 1.5, 1.5, 1, 1, 1];
+            var strs = ["Q1", "Q2", "Q3", "Q4", "OT",
+                        "Own goal line", "20yd", "Red zone"];
+            var align = ["center", "center", "center", "center", "center",
+                         "right", "right", "right"];
+            for (var i=0; i<pts.length; ++i) {
+                Facet.Scene.add(Facet.Text.string_batch({
+                    font: font,
+                    string: strs[i],
+                    size: scale[i],
+                    align: align[i],
+                    position: function(p) {
+                        return interactor.camera(p.add(pts[i]));
+                    },
+                    color: function(p) {
+                        return Shade.vec(0,0,0,1);
+                    }
+                }));
+            }
+        });
 
         Facet.Scene.add(Facet.Marks.dots({
             position: interactor.camera(pt),
