66f51127309dbb66ba999804e703be0802e7506a
Carlos Scheidegger <cscheid@research.att.com>
2013-01-05 21:31:06 -0500

    now all plays. no selection yet

--- main.js
+++ main.js
@@ -24,102 +24,85 @@ $().ready(function() {
         clearColor: [1,1,1,1],
         interactor: interactor
     });
-    Facet.Net.json("data/passes_array.json", function(data) {
+    Facet.Net.json("data/plays.json", function(data) {
         data = new Float32Array(data);
         var secs = Facet.attribute_buffer({
             vertex_array: data,
             item_size: 1,
-            stride: 32,
-            offset: 4
+            stride: 28,
+            offset: 0
         });
-        var yards = Facet.attribute_buffer({
+        var ydline = Facet.attribute_buffer({
             vertex_array: data,
             item_size: 1,
-            stride: 32,
-            offset: 16
+            stride: 28,
+            offset: 12
         });
-        var completed = Facet.attribute_buffer({
-            vertex_array: data,
-            item_size: 1,
-            stride: 32,
-            offset: 24
-        });
-        var intercepted = Facet.attribute_buffer({
-            vertex_array: data,
-            item_size: 1,
-            stride: 32,
-            offset: 20
-        });
-        var yardage = Facet.attribute_buffer({
-            vertex_array: data,
-            item_size: 1,
-            stride: 32,
-            offset: 28
-        });
+
         var pt = Shade.vec(
             Shade(3600).sub(secs).div(3600).mul(100),
-            Shade(100).sub(yards).div(100).mul(20)
+            Shade(100).sub(ydline).div(100).mul(20)
             );
 
-        var unselected_color = Shade.vec(0,0,0,0.1);
-        var selected_color = Shade.parameter("vec4", vec.make([1,0,0,0.5]));
-        var selection_enabled = Shade.parameter("float", 0);
+        // var unselected_color = Shade.vec(0,0,0,0.1);
+        // var selected_color = Shade.parameter("vec4", vec.make([1,0,0,0.5]));
+        // var selection_enabled = Shade.parameter("float", 0);
 
-        var sel_completed   = Shade.parameter("float", 0);
-        var sel_intercepted = Shade.parameter("float", 0);
-        var sel_bias        = Shade.parameter("float", 0);
+        // var sel_completed   = Shade.parameter("float", 0);
+        // var sel_intercepted = Shade.parameter("float", 0);
+        // var sel_bias        = Shade.parameter("float", 0);
 
-        function make_selection(vec) {
-            return function() {
-                sel_completed.set(vec.completed);
-                sel_intercepted.set(vec.intercepted);
-                sel_bias.set(vec.bias * 0.9);
-                selection_enabled.set(1);
-                Facet.Scene.invalidate();
-            };
-        }
+        // function make_selection(vec) {
+        //     return function() {
+        //         sel_completed.set(vec.completed);
+        //         sel_intercepted.set(vec.intercepted);
+        //         sel_bias.set(vec.bias * 0.9);
+        //         selection_enabled.set(1);
+        //         Facet.Scene.invalidate();
+        //     };
+        // }
 
-        function remove_selection() {
-            selection_enabled.set(0);
-            Facet.Scene.invalidate();
-        }
+        // function remove_selection() {
+        //     selection_enabled.set(0);
+        //     Facet.Scene.invalidate();
+        // }
 
-        var hover_out_function = remove_selection;
-        var previous_selection;
+        // var hover_out_function = remove_selection;
+        // var previous_selection;
 
-        function fix_selection(fun) {
-            return function(evt) {
-                if (hover_out_function !== fun) {
-                    $(evt.target).addClass("selected");
-                    hover_out_function = fun;
-                    previous_selection = evt.target;
-                } else {
-                    $(previous_selection).removeClass("selected");
-                    previous_selection = undefined;
-                    hover_out_function = remove_selection;
-                }
-            };
-        }
+        // function fix_selection(fun) {
+        //     return function(evt) {
+        //         if (hover_out_function !== fun) {
+        //             $(evt.target).addClass("selected");
+        //             hover_out_function = fun;
+        //             previous_selection = evt.target;
+        //         } else {
+        //             $(previous_selection).removeClass("selected");
+        //             previous_selection = undefined;
+        //             hover_out_function = remove_selection;
+        //         }
+        //     };
+        // }
 
-        var intercepted_selection = make_selection({completed:  0, intercepted: 1, bias:  1});
-        var complete_selection    = make_selection({completed:  1, intercepted: 0, bias:  1});
-        var incomplete_selection  = make_selection({completed: -1, intercepted: 0, bias: -1});
+        // var intercepted_selection = make_selection({completed:  0, intercepted: 1, bias:  1});
+        // var complete_selection    = make_selection({completed:  1, intercepted: 0, bias:  1});
+        // var incomplete_selection  = make_selection({completed: -1, intercepted: 0, bias: -1});
 
-        $("#intercepted").hover(intercepted_selection, function() { hover_out_function(); });
-        $("#complete")   .hover(complete_selection,    function() { hover_out_function(); });
-        $("#incomplete") .hover(incomplete_selection,  function() { hover_out_function(); });
+        // $("#intercepted").hover(intercepted_selection, function() { hover_out_function(); });
+        // $("#complete")   .hover(complete_selection,    function() { hover_out_function(); });
+        // $("#incomplete") .hover(incomplete_selection,  function() { hover_out_function(); });
 
-        $("#intercepted").click(fix_selection(intercepted_selection));
-        $("#complete")   .click(fix_selection(complete_selection));
-        $("#incomplete") .click(fix_selection(incomplete_selection));
+        // $("#intercepted").click(fix_selection(intercepted_selection));
+        // $("#complete")   .click(fix_selection(complete_selection));
+        // $("#incomplete") .click(fix_selection(incomplete_selection));
 
-        var sel_discriminant = 
-            sel_completed.mul(completed).add(sel_intercepted.mul(intercepted));
+        // var sel_discriminant = 
+        //     sel_completed.mul(completed).add(sel_intercepted.mul(intercepted));
 
-        var selection_predicate = sel_discriminant.gt(sel_bias);
-        var is_selected = selection_enabled.eq(1).and(selection_predicate);
+        // var selection_predicate = sel_discriminant.gt(sel_bias);
+        // var is_selected = selection_enabled.eq(1).and(selection_predicate);
 
-        var final_color = Shade.ifelse(is_selected, selected_color, unselected_color);
+        // var final_color = Shade.ifelse(is_selected, selected_color, unselected_color);
 
         Facet.Scene.add(Facet.Marks.lines({
             elements: 10,
@@ -164,10 +147,11 @@ $().ready(function() {
 
         Facet.Scene.add(Facet.Marks.dots({
             position: interactor.camera(pt),
-            fill_color: final_color,
+            fill_color: Shade.vec(0,0,0,0.1),
             stroke_width: -1,
-            point_diameter: Shade.ifelse(is_selected, interactor.zoom.mul(100).pow(0.666).mul(2), interactor.zoom.mul(100).pow(0.666)),
-            elements: data.length/8
+            point_diameter: interactor.zoom.mul(100).pow(0.666),
+//            Shade.ifelse(is_selected, interactor.zoom.mul(100).pow(0.666).mul(2), interactor.zoom.mul(100).pow(0.666)),
+            elements: data.length/7
         }));
     });
 });
