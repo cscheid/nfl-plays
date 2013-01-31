adce74b188a4925d9554e85994a296993c95bace
Carlos Scheidegger <cscheid@research.att.com>
2013-01-31 11:32:00 -0500

    clickable selections, again, now via d3

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ $().ready(function() {
     interactor = Facet.UI.center_zoom_interactor({
         width: width, height: height, zoom: 2/3
     });
-
     var interactor = Facet.UI.center_zoom_interactor({
         width: width,
         height: height,
@@ -26,56 +25,92 @@ $().ready(function() {
         clearColor: [1,1,1,1],
         interactor: interactor
     });
-    Facet.Net.json("data/sorted_plays.json", function(full_data) {
-        var pointsize = Shade.parameter("float", 0);
-        var pointweight = Shade.parameter("float", 0);
-        Facet.UI.parameter_slider({ element: "#pointsize",   parameter: pointsize,   min: -3, max: 3 });
-        Facet.UI.parameter_slider({ element: "#pointweight", parameter: pointweight, min: -3, max: 3 });
 
-        Facet.Scene.add(Facet.Marks.lines({
-            elements: 10,
-            position: function(index, which) { 
-                var x = Shade.ifelse(which.eq(0), 0, 100);
-                return interactor.camera(Shade.vec(x, index.mul(2)));
-            },
-            color: function() { return Shade.vec(0,0,0,0.2); }
-        }));
+    var pointsize = Shade.parameter("float", 0);
+    var pointweight = Shade.parameter("float", 0);
+    Facet.UI.parameter_slider({ element: "#pointsize",   parameter: pointsize,   min: -3, max: 3 });
+    Facet.UI.parameter_slider({ element: "#pointweight", parameter: pointweight, min: -3, max: 3 });
 
-        Facet.Net.json("data/opensans.regular.json", function(font) {
-            var pts = [
-                Shade.vec(0,20.5),
-                Shade.vec(25,20.5),
-                Shade.vec(50,20.5),
-                Shade.vec(75,20.5),
-                Shade.vec(100,20.5),
-                Shade.vec(-0.2,0-0.5),
-                Shade.vec(-0.2,4-0.5),
-                Shade.vec(-0.2,16-0.5)
-            ];
-            var scale = [1.5, 1.5, 1.5, 1.5, 1.5, 1, 1, 1];
-            var strs = ["1Q", "2Q", "3Q", "4Q", "OT",
-                        "Own goal line", "20yd", "Red zone"];
-            var align = ["center", "center", "center", "center", "center",
-                         "right", "right", "right"];
-            for (var i=0; i<pts.length; ++i) {
-                Facet.Scene.add(Facet.Text.string_batch({
-                    font: font,
-                    string: strs[i],
-                    size: scale[i],
-                    align: align[i],
-                    position: function(p) {
-                        return interactor.camera(p.add(pts[i]));
-                    },
-                    color: function(p) {
-                        return Shade.vec(0,0,0,1);
-                    }
-                }));
-            }
+    Facet.Scene.add(Facet.Marks.lines({
+        elements: 10,
+        position: function(index, which) { 
+            var x = Shade.ifelse(which.eq(0), 0, 100);
+            return interactor.camera(Shade.vec(x, index.mul(2)));
+        },
+        color: function() { return Shade.vec(0,0,0,0.2); }
+    }));
+
+    Facet.Net.json("data/opensans.regular.json", function(font) {
+        var pts = [
+            Shade.vec(0,20.5),
+            Shade.vec(25,20.5),
+            Shade.vec(50,20.5),
+            Shade.vec(75,20.5),
+            Shade.vec(100,20.5),
+            Shade.vec(-0.2,0-0.5),
+            Shade.vec(-0.2,4-0.5),
+            Shade.vec(-0.2,16-0.5)
+        ];
+        var scale = [1.5, 1.5, 1.5, 1.5, 1.5, 1, 1, 1];
+        var strs = ["1Q", "2Q", "3Q", "4Q", "OT",
+                    "Own goal line", "20yd", "Red zone"];
+        var align = ["center", "center", "center", "center", "center",
+                     "right", "right", "right"];
+        for (var i=0; i<pts.length; ++i) {
+            Facet.Scene.add(Facet.Text.string_batch({
+                font: font,
+                string: strs[i],
+                size: scale[i],
+                align: align[i],
+                position: function(p) {
+                    return interactor.camera(p.add(pts[i]));
+                },
+                color: function(p) {
+                    return Shade.vec(0,0,0,1);
+                }
+            }));
+        }
+    });
+
+    var play_kinds = [
+        { id: 0, caption:  "Kickoffs" },
+        { id: 1, caption:  "On-side kicks" },
+        { id: 4, caption:  "Punts" },
+        { id: 6, caption:  "Field Goals" },
+        { id: 2, caption:  "Passes" },
+        { id: 3, caption:  "Rushes" },
+        { id: 5, caption:  "PATs" },
+        { id: 7, caption:  "Sacks" },
+        { id: 8, caption:  "Two-point Attempts" },
+        { id: 9, caption:  "Kneels" },
+        { id: 11, caption: "Spikes" },
+        { id: 10, caption: "Penalty before Snap" },
+        { id: 12, caption: "Fumbled Snap Handoff" },
+        { id: 13, caption: "Under Review" }
+    ];
+
+    d3.select("#play-kinds-div")
+        .selectAll("span")
+        .data(play_kinds)
+        .enter()
+        .append("span")
+        .attr("id", function(play_kind) { return "kind-" + play_kind.id; })
+        .attr("class", "over-fullscreen query-button")
+        .text(function(play_kind) { return play_kind.caption; })
+        .on("click", function(play_kind) {
+            if (all_plays.toggle(play_kind.id)) {
+                d3.select(this)
+                    .attr("class", "over-fullscreen query-button selected");
+            } else {
+                d3.select(this)
+                    .attr("class", "over-fullscreen query-button");
+            };
         });
 
+    Facet.Net.json("data/sorted_plays.json", function(full_data) {
         var parameters_by_kind =
             [ 
-                { selected_color: Shade.color("#ff0000", 0.2),  unselected_color: Shade.vec(0,0,0,0.02), diameter: 2},
+                { selected_color: Shade.color("#1f77b4", 0.5),  unselected_color: Shade.vec(0,0,0,0.02), diameter: 2},
                 { selected_color: Shade.color("#1f77b4", 0.5),  unselected_color: Shade.vec(0,0,0,0.02), diameter: 3},
                 { selected_color: Shade.color("#1f77b4", 0.2),  unselected_color: Shade.vec(0,0,0,0.01), diameter: 1.5},
                 { selected_color: Shade.color("#1f77b4", 0.2),  unselected_color: Shade.vec(0,0,0,0.01), diameter: 1.5},
@@ -115,12 +150,6 @@ $().ready(function() {
                 offset: 12
             });
             var kind = Shade(i);
-            // var column6 = Facet.attribute_buffer({
-            //     vertex_array: data,
-            //     item_size: 1,
-            //     stride: 28,
-            //     offset: 24
-            // });
             
             var pt = Shade.vec(
                 Shade(3600).sub(secs).div(3600).mul(100),
@@ -128,71 +157,21 @@ $().ready(function() {
             );
 
             var unselected_color = Shade.parameter("vec4", Shade.vec(0,0,0,0.05));
-            var selected_color = Shade.parameter("vec4", Shade.color("#1f77b4", 0.2));
             var selection_enabled = Shade.parameter("float", 0);
 
-            // var sel_kind   = Shade.parameter("float", 0);
-            // function make_kind_selection(attrs) {
-            //     return function() {
-            //         diameter_multiplier.set(attrs.diameter);
-            //         selected_color.set(attrs.selected_color);
-            //         unselected_color.set(attrs.unselected_color);
-            //         sel_kind.set(attrs.kind);
-            //         selection_enabled.set(1);
-            //         Facet.Scene.invalidate();
-            //     };
-            // }
-
-            // function remove_selection() {
-            //     selection_enabled.set(0);
-            //     Facet.Scene.invalidate();
-            // }
-
-            // var hover_out_function = remove_selection;
-            // var previous_selection;
-
-            // function fix_selection(fun) {
-            //     return function(evt) {
-            //         if (previous_selection) {
-            //             $(previous_selection).removeClass("selected");
-            //             previous_selection = undefined;
-            //         }
-            //         if (hover_out_function !== fun) {
-            //             $(evt.target).addClass("selected");
-            //             hover_out_function = fun;
-            //             previous_selection = evt.target;
-            //         } else
-            //             hover_out_function = remove_selection;
-            //     };
-            // }
-
-            // var diameter_multiplier = Shade.parameter("float", 2);
-            // var selected_field_goal_color = Shade.color("#1f77b4", 0.2);
-
-            // _.each(,
-            //        function(attrs) {
-            //            var selection = make_kind_selection(attrs);
-            //            $("#kind-" + attrs.kind)
-            //                .hover(selection, function() { hover_out_function(); })
-            //                .click(fix_selection(selection));
-            //        });
-
-            // var is_selected = selection_enabled.eq(1).and(sel_kind.eq(kind));
-
-            // var final_color = Shade.ifelse(selection_enabled.eq(0), Shade.vec(0,0,0,0.1),
-            //                                Shade.ifelse(sel_kind.eq(kind), selected_color, unselected_color));
-
-            var final_color = parameters_by_kind[i].selected_color;
+            var is_selected = selection_enabled.eq(1);
+            var final_color = is_selected.ifelse(parameters_by_kind[i].selected_color, Shade.vec(0,0,0,Shade.mul(0.1, pointweight.exp())));
 
             var base_diameter = interactor.zoom.mul(100).pow(0.666).mul(pointsize.exp());
-            var multiplier = parameters_by_kind[i].diameter; // Shade.ifelse(is_selected, diameter_multiplier, 1);
+            var multiplier = Shade.ifelse(is_selected, parameters_by_kind[i].diameter, 1);
             var final_diameter = base_diameter.mul(multiplier);
 
             return {
                 kind: i,
+                is_selected: selection_enabled,
                 dots: Facet.Marks.dots({
                     position: interactor.camera(pt),
-                    fill_color: final_color.mul(Shade.vec(1,1,1,pointweight.exp())),
+                    fill_color: final_color,
                     stroke_width: -1,
                     point_diameter: final_diameter,
                     elements: data.length/7
@@ -200,24 +179,47 @@ $().ready(function() {
             };
         });
         function find(collection, filter) {
-            for (var i = 0; i < filter.length; i++) {
+            for (var i = 0; i < collection.length; i++) {
                 if (filter(collection[i], i, collection)) return i;
             }
             return -1;
         }
         all_plays = {
+            find: function(kind) {
+                var i = find(plays_by_kind, function(play) { return play.kind === kind; });
+                if (i === -1) throw "booo";
+                return i;
+            },
+            select: function(kind) {
+                var i = this.find(kind);
+                var this_play = plays_by_kind[i];
+                plays_by_kind.splice(i, 1);
+                plays_by_kind.push(this_play);
+                this_play.is_selected.set(1);
+                Facet.Scene.invalidate();
+            },
+            unselect: function(kind) {
+                var this_play = plays_by_kind[this.find(kind)];
+                this_play.is_selected.set(0);
+                Facet.Scene.invalidate();
+            },
+            toggle: function(kind) {
+                var this_play = plays_by_kind[this.find(kind)];
+                if (this_play.is_selected.get()) {
+                    this.unselect(kind);
+                    return false;
+                } else {
+                    this.select(kind);
+                    return true;
+                }
+            },
+
+            //////////////////////////////////////////////////////////////////
+
             draw: function() {
                 _.each(plays_by_kind, function(play) {
                     play.dots.draw();
                 });
-            },
-            select: function(kind) {
-                var i = find(plays_by_kind, function(play) { return play.kind === kind; });
-                if (i === -1) throw "booo";
-                var this_play = plays_by_kind[i];
-                plays_by_kind.splice(i, 1);
-                plays_by_kind.push(this_play);
-                Facet.Scene.invalidate();
             }
         };
         Facet.Scene.add(all_plays);
